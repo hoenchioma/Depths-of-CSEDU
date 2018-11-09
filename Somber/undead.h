ab4d4a2b9f49bcf2ade8360ff474e8e1c6b4398c
farhankabir12 <43378296+farhankabir12@users.noreply.github.com>
2018-11-10 02:00:20 +0600

    Boss2 zombie resoources added

--- Somber/undead.h
+++ Somber/undead.h
@@ -8,8 +8,8 @@ public:
 	Undead(sf::Texture *texture1, sf::Texture *texture2, sf::Texture *texture3);
 
 	sf::Sprite object;
+	AniSprite a[4];
 	int health;
 	int maxHealth;
 	int position;
-	float preX, preY;
 };
