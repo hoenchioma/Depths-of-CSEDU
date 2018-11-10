aa058ada84d1ce40594d7591ac3ffa319736f1bf
farhankabir12 <43378296+farhankabir12@users.noreply.github.com>
2018-11-10 12:24:44 +0600

    added perks effects in boss1

--- Somber/DefaultInv.h
+++ Somber/DefaultInv.h
@@ -12,10 +12,10 @@ void makeDefault(Inventory& inv)
 		{"boss2Complete", 0},
 		{"boss3Complete", 0},
 		// perks
-		{"speed", 0},
-		{"invincible", 0},
+		{"speed", 1},
+		{"invincible", 1},
 		{"healthBoost", 0},
-		{"reLife", 0},
+		{"reLife", 1},
 		{"timeFreeze", 0},
 		{"phaze", 0}
 	};
